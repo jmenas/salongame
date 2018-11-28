e3f7681168ead50fd9911d449f9df2b09630640f
jmenas <joshuamenas@gmail.com>
2018-11-28 18:31:29 -0500

    test upload

---
+++ Josh_Layouts/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1543447591,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/salonbg-sheet0.png",
+		"images/button_test-sheet0.png",
+		"images/scrolltosprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
